f0169a12bffe453506d0255df68eb107b2292839
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-01 23:01:27 +0530

    fixed bugs

--- react-website1/src/App.js
+++ react-website1/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Hero from "./Hero";
+import Appsection from "./Appsection";
+import Card from "./Card";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Appsection />
+      <Card />
       <Footer />
     </div>
   );
