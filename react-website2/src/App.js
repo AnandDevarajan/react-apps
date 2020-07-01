d7999e3d5e409498d231b2d10c5c4e47a679f8e6
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-02 00:11:19 +0530

    navbar added

---
+++ react-website2/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./css/styles.css";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
