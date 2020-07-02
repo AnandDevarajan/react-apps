b8b8f575333475454866ecc3a7c1402d3d83d971
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-02 20:11:04 +0530

    getting winners done

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -27,11 +27,43 @@ const App = () => {
       itemArray[3] !== "empty"
     ) {
       setWinMessage(`${itemArray[3]} wins`);
-    }else if(
+    } else if (
       itemArray[6] === itemArray[7] &&
       itemArray[6] === itemArray[8] &&
       itemArray[8] !== "empty"
-    )
+    ) {
+      setWinMessage(`${itemArray[6]} wins`);
+    } else if (
+      itemArray[0] === itemArray[3] &&
+      itemArray[0] === itemArray[6] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[1] === itemArray[4] &&
+      itemArray[1] === itemArray[7] &&
+      itemArray[1] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[1]} wins`);
+    } else if (
+      itemArray[2] === itemArray[5] &&
+      itemArray[2] === itemArray[8] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    } else if (
+      itemArray[0] === itemArray[4] &&
+      itemArray[0] === itemArray[8] &&
+      itemArray[0] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[0]} wins`);
+    } else if (
+      itemArray[2] === itemArray[4] &&
+      itemArray[2] === itemArray[6] &&
+      itemArray[2] !== "empty"
+    ) {
+      setWinMessage(`${itemArray[2]} wins`);
+    }
   };
   const changeItem = (itemNumber) => {
     if (winMessage) {
