c4077adb607cad1180fece473a744d7c1e55a567
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-04 22:15:23 +0530

    Api fetched

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {} from "dotenv/config";
 import "./App.css";
 
 const App = () => {
   const APP_ID = process.env.APP_ID;
   const APP_KEY = process.env.APP_KEY;
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {}, []);
+
+  const getRecepies = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-button">
+          search
+        </button>
+      </form>
     </div>
   );
 };
