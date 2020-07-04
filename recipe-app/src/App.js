aed0b3981793c71422b92737e1ffbe095e964043
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-04 23:14:37 +0530

    added styles

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -45,14 +45,17 @@ const App = () => {
           search
         </button>
       </form>
-      {recipes.map((recipe) => (
-        <Recipe
-          key={recipe.recipe.calories}
-          title={recipe.recipe.label}
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-        />
-      ))}
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            key={recipe.recipe.calories}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
 };
