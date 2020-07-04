47ed2268c7d4ffae4d4783e2931528c28e891046
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-04 22:39:40 +0530

    added props

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 import {} from "dotenv/config";
+import Recipe from "./Recipe ";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "660a9f90";
   const APP_KEY = "e84a7b33cb7513e8d36d106d2a7c0758";
 
+  const [recipes, setRecipes] = useState([]);
+
   useEffect(() => {
-    getRecepies();
+    getRecipes();
   }, []);
 
-  const getRecepies = async () => {
+  const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
     const data = await response.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits);
   };
 
   return (
@@ -26,6 +30,13 @@ const App = () => {
           search
         </button>
       </form>
+      {recipes.map((recipe) => (
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
+      ))}
     </div>
   );
 };
