23d9d8fce1ea9f7522c084ed3b49265b81d74bc3
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-04 20:35:45 +0530

    got api

---
+++ recipe-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {} from "dotenv/config";
+import "./App.css";
+
+const App = () => {
+  const APP_ID = process.env.APP_ID;
+  const APP_KEY = process.env.APP_KEY;
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  );
+};
+
+export default App;
