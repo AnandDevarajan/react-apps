c2fd5beef47fd03b74a2ca4619ef5f5b23ad8942
Anand Devarajan <ananddevarajan01@gmail.com>
2020-07-09 01:46:21 +0530

    minor bug fixed

--- github-firebase/src/Config/FirebaseConfig.js
+++ github-firebase/src/Config/FirebaseConfig.js
@@ -1,10 +1,10 @@
 export default {
-  apiKey: "AIzaSyDRgKFUk-Vv7HROpPHyZMEJiYTRtQ79jdY",
-  authDomain: "mygitapp-1bdfd.firebaseapp.com",
-  databaseURL: "https://mygitapp-1bdfd.firebaseio.com",
-  projectId: "mygitapp-1bdfd",
-  storageBucket: "mygitapp-1bdfd.appspot.com",
-  messagingSenderId: "432940094053",
-  appId: "1:432940094053:web:266a10128fca2aefedac8c",
-  measurementId: "G-KSZ9L15Q6X",
+  apiKey: process.env.REACT_APP_APP_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABAS_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
