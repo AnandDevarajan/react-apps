681a8bf5741bbc6d21e275d5ea0e89d679cef550
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-05-19 14:45:51 +0530

    Add contact page added

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -1,11 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import { Form, Row, Col, Button, Container, Card } from "react-bootstrap";
 const AddContact = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container fluid className="mt-5">
+      <Card className="px-3 py-3 ml-auto mr-auto" style={{ width: "40rem" }}>
+        <Card.Header as="h1" className="text-center text-info">
+          Add Contact
+        </Card.Header>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              style={{
+                borderRight: "none",
+                borderLeft: "none",
+                borderTop: "none",
+              }}
+            />
+          </Form.Group>
 
-export default AddContact
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Phone Number"
+              style={{
+                borderRight: "none",
+                borderLeft: "none",
+                borderTop: "none",
+              }}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Add
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
+
+export default AddContact;
