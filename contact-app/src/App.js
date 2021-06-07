d35b30d60ff994212bd3e9c7f0fef472b72637e5
Anand Devarajan <45593286+AnandDevarajan@users.noreply.github.com>
2021-06-07 13:29:56 +0530

    local storage

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Footer from "./components/Footer";
 const App = () => {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
   const addContactHandler = (contact) => {
-    console.log(contact);
     setContacts([...contacts, contact]);
   };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) {
+      setContacts(retriveContacts);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div>
